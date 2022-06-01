37bd5330a9b9efe65ca0aff32ac0405d7866b6ce
anthonymike25 <101486774+anthonymike25@users.noreply.github.com>
2022-05-31 19:52:08 -0500

    Actividad_C2

---
+++ Actividad_E2/anthonygarcia.compara.cpp
@@ -0,0 +1,35 @@
+//================================================
+//==> Nombre del programa: compara
+//==> Archivo : compara.cpp
+//==>Autor: Anthony Garcia
+//==>Fecha de elaboración: 2022-05-02
+//==>Fecha ultima actualización: 2022-05-20
+//=================================================
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  float ag_a,ag_b;
+	cout<<"ingrese los valores a,b:"; cin>>ag_a>>ag_b;
+	if(ag_a==ag_b) {
+		cout<<"son iguales:";
+	}else
+
+cout<<endl<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa: compara"<<endl;
+cout<<"//==> Archivo : compara.cpp"<<endl;
+cout<<"//==>Autor: Anthony Garcia"<<endl;
+cout<<"//==>Fecha de elaboración: 2022-05-02"<<endl;
+cout<<"//==>Fecha ultima actualización: 2022-05-20"<<endl;
+cout<<"//================================================="<<endl;
+
+
+    if(ag_a>ag_b) { cout<<"a es mayor";
+		}else{ cout<<"b es mayor";
+		}
+    
+  return 0;
+  
+}
